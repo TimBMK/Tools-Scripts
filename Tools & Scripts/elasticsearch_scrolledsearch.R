81c213f5a8a23d08fa5440355a46eb3cb495436a
TimBMK <tim.b.m.koenig@web.de>
2024-08-21 13:23:28 +0200

    added scrolled search with immediate write to disk

--- Tools & Scripts/elasticsearch_scrolledsearch.R
+++ Tools & Scripts/elasticsearch_scrolledsearch.R
@@ -54,7 +54,7 @@ full_scroll <- function(conn,  # connection to elastic
       }
       if (is.null(list_length)) {
         if (hits == 0) {
-          out <- data.table::rbindlist(list, fill = T)
+          out <- data.table::rbindlist(c(list(out), list), fill = T)
           list <- list()
         }
       } else {
@@ -106,3 +106,111 @@ full_scroll <- function(conn,  # connection to elastic
   return(out)
 }
 
+
+
+
+# this version saves the bound data of every step (list length) - more stable for large datasets
+
+full_scroll_save <- function(
+  conn,  # connection to elastic
+  q = NULL, # Search query
+  body = list(),  # query body. set return values here
+  file_name, # name of the file to write to
+  delim = ",", # delimiter
+  index = NULL,         # Search index
+  size = 10000,         # number of results per Search (result DF size). Balance with list_length. Maximum = 10.000
+  time_scroll = "1h",   # length of scroll id kept alive (see Search() documentation)
+  limit = NULL,         # maximum number of results (NULL for unlimited/everything)
+  ...) {                # additional arguments passed to elastic::Search()
+
+  require(progress)
+  require(elastic)
+  require(data.table)
+  require(vroom)
+
+  if (file.exists(file_name)) stop("File already exists.") # else the data would be appended to the same file
+
+  if (!is.null(limit) && limit < size) {size <- limit} # set size of the results per search to limit if needed, so no more than the limit are returned
+
+  # initial search, sets query
+  res <- elastic::Search(conn, q = q, asdf = T, size = size, body = body, time_scroll = time_scroll, index = index, ...)
+  
+  # limits
+  if (is.null(limit)){
+    total <- res$hits$total$value
+  }
+
+  if (!is.null(limit)){
+    if (res$hits$total$value > limit) {    # if the limit is higher than the actual hits, use maximum hits as ceiling
+      print(paste("Returning", limit, "out of", res$hits$total$value, "search results."))
+      total <- limit
+    } else {
+      print("Limit is higher than actual search results. Returning all search hits.")
+      total <- res$hits$total$value
+    }
+  }
+
+  # progress bar to track download progress and total time elapsed
+  pb <- progress::progress_bar$new(
+    format = "  downloading [:bar] :current / :total | :elapsedfull",
+    total = total, clear = FALSE, width= 60)
+
+  out <- res$hits$hits
+  count <- nrow(out)
+  
+  # write first data with colnames
+  vroom::vroom_write(x = out, file = file_name,
+                     progress = FALSE,
+                     delim = delim, append = FALSE)
+  
+  if (pb$finished != T) {
+    pb$update(count/total)
+  }
+  
+  hits <- 1
+  
+  if (is.null(limit)){
+    while (hits != 0) {
+      res <- elastic::scroll(conn = conn, x = res$`_scroll_id`, asdf = T)
+      hits <- length(res$hits$hits)
+      
+      if (hits > 0) {
+        out <- data.table::rbindlist(list(out[1,], res$hits$hits), # bind with last results for same cols, remove the one row from the last results
+                                     fill = TRUE)[-1,]
+          
+        
+        vroom::vroom_write(x = out, file = file_name,
+                           progress = FALSE,
+                           delim = delim, append = TRUE) ## append = keep writing into the same file
+      } 
+      
+      count <- ifelse(hits != 0, count + nrow(out), count)
+      if (pb$finished != T) {
+        pb$update(count/total)
+      }
+    }
+  }
+  
+
+  if (!is.null(limit)){
+    while ((hits != 0) & (count < limit)) {
+      res <- elastic::scroll(conn = conn, x = res$`_scroll_id`, asdf = T)
+      hits <- length(res$hits$hits)
+      
+      if (hits > 0) {
+        out <- data.table::rbindlist(list(out[1,], res$hits$hits), # bind with last results for same cols, remove the one row from the last results
+                                     fill = TRUE)[-1,]
+        
+        
+        vroom::vroom_write(x = out, file = file_name,
+                           progress = FALSE,
+                           delim = delim, append = TRUE) ## append = keep writing into the same file
+      } 
+      
+      count <- ifelse(hits != 0, count + nrow(out), count)
+      if (pb$finished != T) {
+        pb$update(count/total)
+      }
+    }
+  }
+}
